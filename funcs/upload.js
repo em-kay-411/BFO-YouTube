cd0fc7d02b04e4cf59f7ea4f72098b4ce3fec91b
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2023-09-22 16:02:41 +0530

    README

--- funcs/upload.js
+++ funcs/upload.js
@@ -15,25 +15,4 @@ const upload = multer({
   }),
 });
 
-// // Define a function to upload files to S3
-// function uploadFiles(req, res, next) {
-//     // Use the 'upload' Multer middleware to handle the file uploads
-//     upload.array('files', 5)(req, res, function (err) {
-//         if (err) {
-//             return next(err);
-//         }
-
-//         const files = req.files;
-
-//         // Handle form data and files as needed
-//         // For example, you can log the S3 URLs of the uploaded files
-//         files.forEach((file) => {
-//             console.log('File uploaded to S3:', file.location);
-//         });
-
-//         // Call the next middleware or send a response to the client
-//         res.send('Form data received, and files uploaded to S3 successfully.');
-//     });
-// }
-
 module.exports = upload;
