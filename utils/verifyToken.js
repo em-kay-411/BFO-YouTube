6e098e0622ac6a5b0795221e30626a9d0f57bedf
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2023-09-22 11:05:28 +0530

    Token verification errors handled

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -4,12 +4,12 @@ const createError = require('../utils/error.js');
 const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return next(createError(401, "You are not authenticated!"));
+    return res.status(401).json({ message: 'You are not authenticated' });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err){
-        return next(createError(403, "Token is not valid!"));
+        return res.status(403).json({ message: 'Invalid token' });
     }
     req.user = user;
     next();
